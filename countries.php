40d05cf6c9fdf5abfa94ed60e5b0286bf967803a
Mohammed Le Doze <mohammed.ledoze@gmail.com>
2013-12-08 17:53:19 +0100

    prettify JSON source to ease the contributions add JSON converter to minify the source file move generated files to the `dist` folder refactor countries.php: added `save` method

--- countries.php
+++ countries.php
@@ -16,6 +16,12 @@ interface Converter {
 	 * @return string
 	 */
 	public function convert();
+
+	/**
+	 * Save the converted data to the disk
+	 * @return mixed
+	 */
+	public function save();
 }
 
 /**
@@ -29,6 +35,11 @@ abstract class AbstractConverter implements Converter {
 	protected $aCountries;
 
 	/**
+	 * @var string path of the output directory
+	 */
+	protected $sOutputDirectory;
+
+	/**
 	 * @param array $aCountries
 	 */
 	public function __construct(array $aCountries) {
@@ -36,18 +47,58 @@ abstract class AbstractConverter implements Converter {
 	}
 
 	/**
+	 * Save the data to disk
+	 * @param string $sOutputFile name of the output file
+	 * @return int|bool
+	 */
+	public function save($sOutputFile = '') {
+		if (empty($this->sOutputDirectory)) {
+			$this->setDefaultOutputDirectory();
+		}
+		if (!is_dir($this->sOutputDirectory)) {
+			mkdir($this->sOutputDirectory);
+		}
+		if (empty($sOutputFile)) {
+			$sTempFile = date('Ymd-His', time()) . '-countries';
+			$sOutputFile = $sTempFile;
+		}
+		return file_put_contents($this->sOutputDirectory . $sOutputFile, $this->convert());
+	}
+
+	/**
 	 * Converts arrays to comma-separated strings
 	 * @param array $aInput
+	 * @param string $sGlue
 	 * @return array
 	 */
-	protected function convertArrays(array &$aInput) {
-		$aInput = array_map(function ($value) {
+	protected function convertArrays(array &$aInput, $sGlue = ',') {
+		$aInput = array_map(function ($value) use ($sGlue) {
 			if (is_array($value)) {
-				return implode(',', $value);
+				return implode($sGlue, $value);
 			}
 			return $value;
 		}, $aInput);
 		return $aInput;
+	}
+
+	/**
+	 * Set the default output directory
+	 */
+	private function setDefaultOutputDirectory() {
+		$this->sOutputDirectory = __DIR__ . DIRECTORY_SEPARATOR . 'dist' . DIRECTORY_SEPARATOR;
+	}
+}
+
+/**
+ * Class JsonConverter
+ */
+class JsonConverter extends AbstractConverter {
+
+	/**
+	 * @return string minified JSON, one country per line
+	 */
+	public function convert() {
+		return preg_replace("@},{@", "}," . PHP_EOL . "{", json_encode($this->aCountries) . PHP_EOL);
 	}
 }
 
@@ -60,25 +111,19 @@ class CsvConverter extends AbstractConverter {
 	 * @var
 	 */
 	private $sGlue = '";"';
+
 	/**
 	 * @var string
 	 */
 	private $sBody = '';
 
 	/**
-	 * Convert into a new format
-	 * @return string
+	 * @return string data converted into CSV
 	 */
 	public function convert() {
 		array_walk($this->aCountries, array($this, 'processCountry'));
-		return $this->getHeaders() . PHP_EOL . $this->sBody;
-	}
-
-	/**
-	 * @return string
-	 */
-	private function getHeaders() {
-		return !empty($this->aCountries) ? '"' . implode($this->sGlue, array_keys($this->aCountries[0])) . '"' : '';
+		$sHeaders = '"' . implode($this->sGlue, array_keys($this->aCountries[0])) . '"';
+		return $sHeaders . PHP_EOL . $this->sBody;
 	}
 
 	/**
@@ -117,13 +162,14 @@ class XmlConverter extends AbstractConverter {
 	 */
 	public function __construct(array $aCountries) {
 		$this->oDom = new DOMDocument('1.0', 'UTF-8');
+		$this->formatOutput();
+		$this->preserveWhiteSpace();
 		$this->oDom->appendChild($this->oDom->createElement('countries'));
 		parent::__construct($aCountries);
 	}
 
 	/**
-	 * Convert into a new format
-	 * @return string
+	 * @return string data converted into XML
 	 */
 	public function convert() {
 		array_walk($this->aCountries, array($this, 'processCountry'));
@@ -132,6 +178,7 @@ class XmlConverter extends AbstractConverter {
 
 	/**
 	 * @param bool $bFormatOutput
+	 * @see \DOMDocument::$formatOutput
 	 */
 	public function formatOutput($bFormatOutput = true) {
 		$this->oDom->formatOutput = $bFormatOutput;
@@ -139,6 +186,7 @@ class XmlConverter extends AbstractConverter {
 
 	/**
 	 * @param bool $bPreserveWhiteSpace
+	 * @see \DOMDocument::$preserveWhiteSpace
 	 */
 	public function preserveWhiteSpace($bPreserveWhiteSpace = false) {
 		$this->oDom->preserveWhiteSpace = $bPreserveWhiteSpace;
@@ -157,16 +205,7 @@ class XmlConverter extends AbstractConverter {
 	}
 }
 
-$aCountriesSrc = json_decode(file_get_contents('countries.json'), true);
-
-// convert to CSV
-$oCsvConverter = new CsvConverter($aCountriesSrc);
-$sCsvContent = $oCsvConverter->convert();
-file_put_contents('countries.csv', $sCsvContent);
-
-// convert to XML
-$oXmlConverter = new XmlConverter($aCountriesSrc);
-$oXmlConverter->formatOutput();
-$oXmlConverter->preserveWhiteSpace();
-$sXmlContent = $oXmlConverter->convert();
-file_put_contents('countries.xml', $sXmlContent);
+$aCountriesSrc = json_decode(file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . 'countries.json'), true);
+(new JsonConverter($aCountriesSrc))->save('countries.json');
+(new CsvConverter($aCountriesSrc))->save('countries.csv');
+(new XmlConverter($aCountriesSrc))->save('countries.xml');
