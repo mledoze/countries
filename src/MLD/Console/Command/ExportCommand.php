eec7d9193b1889387925b5b3ecb9525e1020947a
Mohammed Le Doze <mohammed.ledoze@gmail.com>
2018-02-11 16:44:36 +0100

    WIP: continue to refactor the export command
    added constants for command options
    moved the logic to save the conversion result in the command
    added more verbose logs

--- src/MLD/Console/Command/ExportCommand.php
+++ src/MLD/Console/Command/ExportCommand.php
@@ -3,17 +3,24 @@
 namespace MLD\Console\Command;
 
 use MLD\Converter\Factory;
+use MLD\Enum\Formats;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
- * Class ExportCommand
+ * Command to export countries data to various formats.
  * @package MLD\Console\Command
  */
 class ExportCommand extends Command
 {
+    const BASE_OUTPUT_FILENAME = 'countries';
+
+    const OPTION_EXCLUDE_FIELD = 'exclude-field';
+    const OPTION_INCLUDE_FIELD = 'include-field';
+    const OPTION_FORMAT = 'format';
+    const OPTION_OUTPUT_DIR = 'output-dir';
 
     /**
      * @var string
@@ -23,33 +30,26 @@ class ExportCommand extends Command
     /**
      * @var string
      */
-    private $defaultOutputDirectory;
+    private $outputDirectory;
 
     /**
-     * @var array
+     * @var Factory
      */
-    private $converters = [
-        'json' => ['class' => '\MLD\Converter\JsonConverter', 'output_file' => 'countries.json'],
-        'json_unescaped' => [
-            'class' => '\MLD\Converter\JsonConverterUnicode',
-            'output_file' => 'countries-unescaped.json'
-        ],
-        'csv' => ['class' => '\MLD\Converter\CsvConverter', 'output_file' => 'countries.csv'],
-        'xml' => ['class' => '\MLD\Converter\XmlConverter', 'output_file' => 'countries.xml'],
-        'yml' => ['class' => '\MLD\Converter\YamlConverter', 'output_file' => 'countries.yml'],
-    ];
+    private $converterFactory;
 
     /**
      * @param string $inputFile Full path and filename of the input country data JSON file.
      * @param string $defaultOutputDirectory Full path to output directory for converted files.
      * @param string|null $name
+     * @throws \Symfony\Component\Console\Exception\LogicException
      */
     public function __construct($inputFile, $defaultOutputDirectory, $name = 'convert')
     {
-        $this->inputFile = $inputFile;
-        $this->defaultOutputDirectory = $defaultOutputDirectory;
-
         parent::__construct($name);
+
+        $this->inputFile = $inputFile;
+        $this->outputDirectory = $defaultOutputDirectory;
+        $this->converterFactory = $this->createConverterFactory();
     }
 
     /**
@@ -60,32 +60,32 @@ class ExportCommand extends Command
         $this
             ->setDescription('Converts source country data to various output formats')
             ->addOption(
-                'exclude-field',
+                self::OPTION_EXCLUDE_FIELD,
                 'x',
                 InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED,
                 'If set, excludes top-level field with the given name from the output. Cannot be used with --include-field',
                 []
             )
             ->addOption(
-                'include-field',
+                self::OPTION_INCLUDE_FIELD,
                 'i',
                 InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED,
                 'If set, include only these top-level fields with the given name from the output. Cannot be used with --exclude-field',
                 []
             )
             ->addOption(
-                'format',
+                self::OPTION_FORMAT,
                 'f',
                 InputOption::VALUE_IS_ARRAY | InputOption::VALUE_REQUIRED,
                 'Output formats',
-                array_keys($this->converters)
+                Formats::getAll()
             )
             ->addOption(
-                'output-dir',
+                self::OPTION_OUTPUT_DIR,
                 null,
                 InputOption::VALUE_OPTIONAL | InputOption::VALUE_REQUIRED,
                 'Directory where you want to put output files',
-                $this->defaultOutputDirectory
+                $this->outputDirectory
             );
     }
 
@@ -96,71 +96,75 @@ class ExportCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $factory = $this->createConverterFactory();
+        $this->setOutputDirectory($input);
+        $this->createOutputDirectory($output);
 
         $countries = $this->decodeInputFile();
-        $countries = $this->filterFields($countries, $input);
+
+        $outputFields = $this->getOutputFields($input, $countries);
+        if ($output->isVerbose()) {
+            $output->writeln(sprintf('Output fields: %s', implode(',', $outputFields)));
+        }
+
+        $countries = $this->filterFields($countries, $outputFields);
 
         /** @var array $formats */
-        $formats = $input->getOption('format');
-        $outputDirectory = $input->getOption('output-dir');
+        $formats = $input->getOption(self::OPTION_FORMAT);
 
         foreach ($formats as $format) {
             if ($output->isVerbose()) {
                 $output->writeln('Converting to ' . $format);
             }
 
-            $converter = $factory->create($format);
+            try {
+                $converter = $this->converterFactory->create($format);
+            } catch (\InvalidArgumentException $exception) {
+                $output->writeln(sprintf("Skipping format '%s': %s", $format, $exception->getMessage()));
+                continue;
+            }
 
             $conversionResult = $converter->convert($countries);
-            // TODO save the result of the conversion
+
+            $filename = $this->generateFilename($format);
+            $this->saveConversion($filename, $conversionResult);
         }
 
-        // TODO move this to another method
-        $output->writeln(
-            sprintf(
-                '<info>Converted data for %d countries into %d formats.</info>',
-                count($countries), count($this->converters)
-            )
-        );
+        $this->printResult($output, $countries, $formats);
     }
 
     /**
      * @param array $countries
-     * @param InputInterface $input
+     * @param array $outputFields
      * @return array
-     * @throws \Symfony\Component\Console\Exception\InvalidArgumentException
      */
-    private function filterFields(array $countries, InputInterface $input)
+    private function filterFields(array $countries, array $outputFields)
     {
-        $baseFields = array_keys(reset($countries));
-        $excludeFields = $input->getOption('exclude-field');
-        $includeFields = $input->getOption('include-field');
-
-        $outputFields = $this->getOutputFields($baseFields, $excludeFields, $includeFields);
-
         if (empty($outputFields)) {
             return $countries;
         }
 
+        $flippedOutputFields = array_flip($outputFields);
         return array_map(
-            function ($country) use ($outputFields) {
-                return array_intersect_key($country, array_flip($outputFields));
+            function ($country) use ($flippedOutputFields) {
+                return array_intersect_key($country, $flippedOutputFields);
             },
             $countries
         );
     }
 
     /**
-     * @param array $baseFields
-     * @param array $excludeFields
-     * @param array $includeFields
+     * @param InputInterface $input
+     * @param array $countries
      * @return array
+     * @throws \Symfony\Component\Console\Exception\InvalidArgumentException
      */
-    private function getOutputFields($baseFields, $excludeFields, $includeFields)
+    private function getOutputFields(InputInterface $input, array $countries)
     {
-        $outputFields = $baseFields;
+        $baseFields = array_keys(reset($countries));
+        $excludeFields = $input->getOption(self::OPTION_EXCLUDE_FIELD);
+        $includeFields = $input->getOption(self::OPTION_INCLUDE_FIELD);
 
+        $outputFields = $baseFields;
         if (!empty($excludeFields)) {
             $outputFields = array_diff($baseFields, $excludeFields);
         } elseif (!empty($includeFields)) {
@@ -184,5 +188,78 @@ class ExportCommand extends Command
     private function decodeInputFile()
     {
         return json_decode(file_get_contents($this->inputFile), true);
+    }
+
+    /**
+     * @param OutputInterface $output
+     * @param array $countries
+     * @param array $formats
+     */
+    private function printResult(OutputInterface $output, array $countries, array $formats)
+    {
+        $formatsCount = count($formats);
+        $output->writeln(
+            sprintf(
+                ngettext(
+                    '<info>Converted data for %d countries into %d format.</info>',
+                    '<info>Converted data for %d countries into %d formats.</info>',
+                    $formatsCount
+                ),
+                count($countries),
+                $formatsCount
+            )
+        );
+    }
+
+    /**
+     * @param string $filename
+     * @param string $conversionResult
+     */
+    private function saveConversion($filename, $conversionResult)
+    {
+        $outputFile = $this->outputDirectory . DIRECTORY_SEPARATOR . $filename;
+        file_put_contents($outputFile, $conversionResult);
+    }
+
+    /**
+     * @param OutputInterface $output
+     */
+    private function createOutputDirectory(OutputInterface $output)
+    {
+        if (is_dir($this->outputDirectory) === false) {
+            if ($output->isVerbose()) {
+                $output->writeln('Creating directory %s', $this->outputDirectory);
+            }
+            mkdir($this->outputDirectory);
+
+            // TODO maybe move this line after the if?
+            $this->outputDirectory = realpath($this->outputDirectory);
+        }
+    }
+
+    /**
+     * @param string $format
+     * @return string
+     */
+    private function generateFilename($format)
+    {
+        $baseFilename = self::BASE_OUTPUT_FILENAME;
+
+        // special case for JSON unespaced
+        if ($format === Formats::JSON_UNESCAPED) {
+            $baseFilename .= '-unescaped';
+            $format = Formats::JSON;
+        }
+
+        return sprintf('%s.%s', $baseFilename, $format);
+    }
+
+    /**
+     * @param InputInterface $input
+     * @throws \Symfony\Component\Console\Exception\InvalidArgumentException
+     */
+    private function setOutputDirectory(InputInterface $input)
+    {
+        $this->outputDirectory = trim($input->getOption(self::OPTION_OUTPUT_DIR)) ?: $this->outputDirectory;
     }
 }
--- src/MLD/Enum/Formats.php
+++ src/MLD/Enum/Formats.php
@@ -10,6 +10,20 @@ class Formats
     const CSV = 'csv';
     const JSON = 'json';
     const JSON_UNESCAPED = 'json_unescaped';
-    const YAML = 'yml';
     const XML = 'xml';
+    const YAML = 'yml';
+
+    /**
+     * @return array
+     */
+    public static function getAll()
+    {
+        return [
+            self::CSV,
+            self::JSON,
+            self::JSON_UNESCAPED,
+            self::XML,
+            self::YAML
+        ];
+    }
 }
