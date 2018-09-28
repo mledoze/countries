3556b5e24a4d273df99b58c7e9fec7985b94d145
Mohammed Le Doze <mohammed.ledoze@gmail.com>
2018-09-28 23:28:35 +0200

    add first unit tests

---
+++ tests/MLDTests/Converter/FactoryTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace MLDTests\Converter;
+
+use MLD\Converter\CsvConverter;
+use MLD\Converter\Factory;
+use MLD\Converter\JsonConverter;
+use MLD\Converter\JsonConverterUnicode;
+use MLD\Converter\XmlConverter;
+use MLD\Converter\YamlConverter;
+use MLD\Enum\Formats;
+use PHPUnit\Framework\TestCase;
+
+class FactoryTest extends TestCase
+{
+
+    /**
+     * @var Factory
+     */
+    private $_factory;
+
+    /**
+     * @inheritDoc
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $this->_factory = new Factory();
+    }
+
+    /**
+     * @dataProvider provideFormats
+     * @param string $format
+     * @param string $expectedConverterClass
+     */
+    public function testCreate(string $format, string $expectedConverterClass): void
+    {
+        $converter = $this->_factory->create($format);
+        $this->assertInstanceOf($expectedConverterClass, $converter);
+    }
+
+    /**
+     * @return \Generator
+     */
+    public function provideFormats(): \Generator
+    {
+        yield 'Format CSV' => [Formats::CSV, CsvConverter::class];
+        yield 'Format JSON' => [Formats::JSON, JsonConverter::class];
+        yield 'Format JSON_UNESCAPED' => [Formats::JSON_UNESCAPED, JsonConverterUnicode::class];
+        yield 'Format XML' => [Formats::XML, XmlConverter::class];
+        yield 'Format YAML' => [Formats::YAML, YamlConverter::class];
+    }
+}
