8c6459c2d4f028977d92e7a0bfab3a7b00c7b8b8
Sascha Müllner <sascha@muellner.de>
2019-08-11 19:22:06 +0200

    Update JsonConverter.php

--- src/MLD/Converter/JsonConverter.php
+++ src/MLD/Converter/JsonConverter.php
@@ -13,17 +13,7 @@ class JsonConverter extends AbstractJsonConverter
      */
     public function convert()
     {
-        /*
         $this->processEmptyArrays();
         return preg_replace("@},{@", "},\n{", json_encode($this->countries) . "\n");
-            */
-            $countries = array();
-            foreach ($this->countries as $key => $value) {
-                $ucid = $key + 1;
-                $value["ucid"] = $ucid;
-                array_push ($countries, $value);
-            }
-            return json_encode($countries);
-
     }
 }
