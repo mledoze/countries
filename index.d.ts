3acaf84b008c66797caa0fc8ce2928f79f04a6d4
Marnus Weststrate <marnusw@gmail.com>
2019-11-19 11:52:28 +0000

    Add types for TypeScript

---
+++ index.d.ts
@@ -0,0 +1,41 @@
+export interface OfficialAndCommon {
+  common: string
+  official: string
+}
+
+export interface CountryName extends OfficialAndCommon {
+  native: {
+    [languageCode: string]: OfficialAndCommon
+  }
+}
+
+export interface Country {
+  name: CountryName
+  tld: string[]
+  cca2: string
+  ccn3: string
+  cca3: string
+  cioc: string
+  independent: boolean
+  status: string
+  currency: string[]
+  callingCode: string[]
+  capital: string[]
+  altSpellings: string[]
+  region: string
+  subregion: string
+  languages: { [languageCode: string]: string }
+  translations: { [languageCode: string]: OfficialAndCommon }
+  latlng: [number, number]
+  demonym: string
+  landlocked: boolean
+  borders: string[]
+  area: number
+  flag: string
+}
+
+export type Countries = Country[]
+
+declare const countries: Countries
+
+export default countries
