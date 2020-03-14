980bf6276c9ca0625154a3b382bf0d422f6d77f8
Reece Daniels <reece.daniels@kwiff.com>
2020-03-14 21:44:03 +0000

    split countries into separate files

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 module.exports = require('./countries.json');
+
+// const { execSync } = require('child_process');
+// const fs = require('fs')
+// const uab = require('unique-array-objects');
+// const jsonToMarkdown = require('json-to-markdown-table')
+// const jsonToTable = require('json-to-table')
+// const mdTable = require('markdown-table')
+
+// const baseDirectory = '/Users/reecedaniels/dev/kwiff/_counting'
+
+// const parsedCountries = JSON.parse(fs.readFileSync(`./countries.json`).toString())
+
+// console.log(parsedCountries)
+
+// parsedCountries.forEach(country => {
+//   fs.writeFileSync(`./country-data/${country.name.common.replace(' ', '-').toLowerCase()}.json`, JSON.stringify(country, null, 4), { encoding: 'utf8' })
+// })
