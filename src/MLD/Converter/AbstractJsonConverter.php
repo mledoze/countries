91ccdce8b536314213bdca5819b715ea98ea903d
Mohammed Le Doze <mledoze@users.noreply.github.com>
2023-09-11 23:04:55 +0200

    refactor build script

--- src/MLD/Converter/AbstractJsonConverter.php
+++ src/MLD/Converter/AbstractJsonConverter.php
@@ -1,6 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace MLD\Converter;
+
+use MLD\Enum\Fields;
+use stdClass;
 
 /**
  * Class AbstractJsonConverter
@@ -9,18 +14,40 @@ abstract class AbstractJsonConverter extends AbstractConverter
 {
 
     /**
+     * @inheritdoc
+     */
+    public function convert(array $countries): string
+    {
+        $countries = $this->processEmptyArrays($countries);
+        return preg_replace(
+            '@},{@',
+            sprintf('},%s{', PHP_EOL),
+            $this->jsonEncode($countries) . PHP_EOL
+        );
+    }
+
+    /**
+     * JSON encode countries data
+     */
+    abstract protected function jsonEncode(array $countries): string;
+
+    /**
      * Special case for empty arrays that should be encoded as empty JSON objects
      */
-    protected function processEmptyArrays()
+    protected function processEmptyArrays(array $countries): array
     {
-        array_walk($this->countries, function (&$country) {
-            if (isset($country['languages']) && empty($country['languages'])) {
-                $country['languages'] = new \stdClass();
-            }
+        return array_map(
+            function ($country) {
+                if (isset($country[Fields::LANGUAGES]) && empty($country[Fields::LANGUAGES])) {
+                    $country[Fields::LANGUAGES] = new stdClass();
+                }
 
-            if (isset($country['name']['native']) && empty($country['name']['native'])) {
-                $country['name']['native'] = new \stdClass();
-            }
-        });
+                if (isset($country[Fields::NAME][Fields::NATIVE]) && empty($country[Fields::NAME][Fields::NATIVE])) {
+                    $country[Fields::NAME][Fields::NATIVE] = new stdClass();
+                }
+                return $country;
+            },
+            $countries
+        );
     }
 }
---
+++ src/MLD/Enum/Fields.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MLD\Enum;
+
+/**
+ * List of all fields in the dataset
+ */
+class Fields
+{
+    public const ALT_SPELLINGS = 'altSpellings';
+    public const AREA = 'area';
+    public const BORDERS = 'borders';
+    public const CALLING_CODE = 'callingCode';
+    public const CAPITAL = 'capital';
+    public const CCA2 = 'cca2';
+    public const CCA3 = 'cca3';
+    public const CCN3 = 'ccn3';
+    public const CIOC = 'cioc';
+    public const CURRENCIES = 'currencies';
+    public const DEMONYM = 'demonym';
+    public const FLAG = 'flag';
+    public const INDEPENDENT = 'independent';
+    public const LANDLOCKED = 'landlocked';
+    public const LANGUAGES = 'languages';
+    public const LAT_LNG = 'latlng';
+    public const NAME = 'name';
+    public const REGION = 'region';
+    public const STATUS = 'status';
+    public const SUBREGION = 'subregion';
+    public const TLD = 'tld';
+    public const TRANSLATIONS = 'translations';
+
+    // Subfields
+    public const NATIVE = 'native';
+
+}
