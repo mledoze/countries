45ac8a5abdc5ee779f0ccee96fed26f5d0432a5c
Mohammed Le Doze <mledoze@users.noreply.github.com>
2023-09-13 00:12:04 +0200

    generate calling codes at build time

--- src/MLD/Enum/ExportCommandOptions.php
+++ src/MLD/Enum/ExportCommandOptions.php
@@ -7,11 +7,11 @@ namespace MLD\Enum;
 /**
  * List the available options for the export command
  */
-class ExportCommandOptions
+enum ExportCommandOptions: string
 {
 
-    public const OUTPUT_DIR = 'output-dir';
-    public const INCLUDE_FIELD = 'include-field';
-    public const EXCLUDE_FIELD = 'exclude-field';
-    public const FORMAT = 'format';
+    case OUTPUT_DIR = 'output-dir';
+    case FORMAT = 'format';
+    case INCLUDE_FIELD = 'include-field';
+    case EXCLUDE_FIELD = 'exclude-field';
 }
