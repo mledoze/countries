4731ae292426ea3e4e27feafa4b66b2d9cd4b9f6
Mohammed Le Doze <mledoze@users.noreply.github.com>
2023-10-07 22:11:56 +0200

    fix: remove native names

--- src/MLD/Converter/CsvConverter.php
+++ src/MLD/Converter/CsvConverter.php
@@ -28,6 +28,7 @@ class CsvConverter extends AbstractConverter
     {
         // special case for currencies, use keys only
         $firstEntry = $this->extractCurrencyCodes($countries[0]);
+        $firstEntry = $this->removeNativeNames($firstEntry);
         $flattenedFirstEntry = $this->flatten($firstEntry);
         return sprintf('"%s"', implode($this->glue, array_keys($flattenedFirstEntry)));
     }
@@ -37,6 +38,7 @@ class CsvConverter extends AbstractConverter
         $lines = array_map(
             function ($country) {
                 $country = $this->extractCurrencyCodes($country);
+                $country = $this->removeNativeNames($country);
                 return sprintf('"%s"', implode($this->glue, $this->flatten($country)));
             },
             $countries
@@ -50,6 +52,17 @@ class CsvConverter extends AbstractConverter
             $country[Fields::CURRENCIES->value] = array_keys($country[Fields::CURRENCIES->value]);
         }
 
+        return $country;
+    }
+
+    /**
+     * Remove `name.native` field to prevent inconsistent column count between countries
+     */
+    private function removeNativeNames(array $country): array
+    {
+        if (isset($country[Fields::NAME->value][Fields::NAME_NATIVE->value])) {
+            unset($country[Fields::NAME->value][Fields::NAME_NATIVE->value]);
+        }
         return $country;
     }
 }
